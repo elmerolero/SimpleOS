11129d29db8ddc0ea658f75fe5db89fd6375ddfb
Ismael Salas López <isma.salas@protonmail.com>
2025-06-12 09:42:33 -0600

    Updated to work in mmu_Init function

--- source/kernel.s
+++ source/kernel.s
@@ -2,49 +2,9 @@
 .global _start
 _start:
     bl      stack_Init
-    //bl      mmu_Init
+    bl      mmu_Init
     //bl      interrupts_Init
     //bl      clock_manager_init
-    
-    /*ldr     r0, =main
-    mov     r1, #16
-    bl      uart0_u32_write
-    mov     r0, #'\n'
-    mov     r1, #0
-    bl      uart0_write
-    mov     r0, #'\r'
-    mov     r1, #0
-    bl      uart0_write*/
-
-    mov     r0, #0xC000      @ Page table start
-    ldr     r1, main_table           @ Gets main table data and saves it
-    str     r1, [ r0 ]
-    mov     r1, #0x10000    @ Second table start
-    ldr     r2, secondary_table_addr @ Gets second table data
-    mov     r4, #0
-1:
-    ldr     r3, [ r2, r4, lsl #2 ]
-    str     r3, [ r1, r4, lsl #2 ]
-    add     r4, r4, #1
-    cmp     r4, #11
-    bls     1b
-
-    mov     r0, #0
-    mcr     p15, 0, r0, c2, c0, 2           @ TTBCR = 0 → usar solo TTBR0
-    mov     r0, #0xC000                     @ Page table start
-    mcr     p15, 0, r0, c2, c0, 0           @ tlb base
-    mov     r0, #0
-    mcr     p15, 0, r0, c8, c7, 0           @ invalidate unified TLB
-    mov     r0, #0xFFFFFFFF
-    mcr     p15, 0, r0, c3, c0, 0           @ DACR = todo en modo manager
-    mrc     p15, 0, r0, c1, c0, 0           @ leer control register
-    mov     r0, #0
-    mcr     p15, 0, r0, c7, c5, 0           @ Invalidate I-cache
-    bic     r0, r0, #(1 << 12)              @ opcional: deshabilita cachés
-    bic     r0, r0, #(1 << 2)
-    bic     r0, r0, #(1 << 28)              @ TEX remap OFF (bit 28 = 0)
-    orr     r0, r0, #MMU_C1_MBIT_ENABLE     @ habilitar MMU (bit 0)
-    mcr     p15, 0, r0, c1, c0, 0           @ escribir control register
     
     ldr     r0, baudrate_speed
     mov     r1, #3
@@ -56,36 +16,6 @@ _start:
 
 baudrate_speed:
     .word 9600
-
-palabra_addr:
-    .word 0x17000
-
-start_main_addr:
-    .word 0xC000
-
-start_second_addr:
-    .word 0x10000
-
-secondary_table_addr:
-    .word secondary_table
-
-main_table: 
-    .word  0x10000 | MMU_L1_COARSE_ENTRY
-
-secondary_table:
-    .word (0x00011000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 0 (VA 0x00001000) (und)
-    .word (0x00012000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 1 (VA 0x00002000) (abt)
-    .word (0x00013000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 2 (VA 0x00003000) (irq)
-    .word (0x00014000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 3 (VA 0x00004000) (fiq)
-    .word (0x00015000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 4 (VA 0x00005000) (sys)
-    .word (0x00016000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 5 (text)
-    .word (0x00017000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 6 (data)
-    .word (0x00007000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 7 (VA 0x00007000) (init_stack)
-    .word (0x00008000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 8 (VA 0x00008000) (.init)
-    .word (0x2000B000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 9 (VA FOR DEVICES TEMPORARY) Interrupts & Mailbox
-    .word (0x20200000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada A (VA FOR DEVICES TEMPORARY) GPIO
-    .word (0x20215000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada B (VA FOR DEVICES TEMPORARY) UART
-    .word (0x20215000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada B (VA FOR DEVICES TEMPORARY) UART
 
 .section .init
 stack_Init:
@@ -149,4 +79,5 @@ loop:
 .include "devices/arm_timer.s"
 .include "devices/clock_manager.s"
 .include "graphics/frame_buffer.s"
+.include "system/mmu/mmu.s"
 .include "system/mmu/constants.s"
--- source/system/mmu/mmu.s
+++ source/system/mmu/mmu.s
@@ -2,46 +2,51 @@
 
 .section .init
 mmu_Init:
-    ldr     r0, start_main_addr      @ Page table start
-    ldr     r1, main_table           @ Gets main table data and saves it
+    push { r4, lr }
+    ldr     r0, level1_table_addr           //Page table start
+    ldr     r1, level1_table_entries        // Gets main table data and saves it
     str     r1, [ r0 ]
-    ldr     r1, start_second_addr    @ Second table start
-    ldr     r2, secondary_table_addr @ Gets second table data
+    ldr     r1, level2_table_addr           //Second table start
+    ldr     r2, level2_table_entries_addr   //Gets second table data
     mov     r4, #0
 1:
     ldr     r3, [ r2, r4, lsl #2 ]
     str     r3, [ r1, r4, lsl #2 ]
     add     r4, r4, #1
-    cmp     r4, #10
+    cmp     r4, #11
     bls     1b
-    mov     r0, #0
-    mcr     p15, 0, r0, c2, c0, 2  @ TTBCR = 0 → usar solo TTBR0
-    ldr     r0, start_main_addr      @ Page table start
-    mcr     p15, 0, r0, c2, c0, 0 @ tlb base
-    mov     r0, #0
-    mcr     p15, 0, r0, c8, c7, 0   @ invalidate unified TLB
-    mov     r0, #0xFFFFFFFF
-    mcr     p15, 0, r0, c3, c0, 0       @ DACR = todo en modo manager
-    mrc     p15, 0, r0, c1, c0, 0   @ leer control register
-    orr     r0, r0, #MMU_C1_MBIT_ENABLE       @ habilitar MMU (bit 0)
-    bic     r0, r0, #(1 << 12)       @ opcional: deshabilita caché
-    bic     r0, r0, #(1 << 2)
-    mcr     p15, 0, r0, c1, c0, 0   @ escribir control register
-    bx      lr
 
-start_main_addr:
+    mov     r0, #0                          // TTBCR = 0 - Use TTBR0 only
+    mcr     p15, 0, r0, c2, c0, 2           
+    mov     r0, #0xC000                     // Page table start
+    mcr     p15, 0, r0, c2, c0, 0           // tlb base
+    mov     r0, #0
+    mcr     p15, 0, r0, c8, c7, 0           // invalidate unified TLB
+    mov     r0, #0xFFFFFFFF
+    mcr     p15, 0, r0, c3, c0, 0           // DACR = todo en modo manager
+    mrc     p15, 0, r0, c1, c0, 0           // leer control register
+    mov     r0, #0
+    mcr     p15, 0, r0, c7, c5, 0           // Invalidate I-cache
+    bic     r0, r0, #(1 << 12)              // opcional: deshabilita cachés
+    bic     r0, r0, #(1 << 2)
+    bic     r0, r0, #(1 << 28)              // TEX remap OFF (bit 28 = 0)
+    orr     r0, r0, #MMU_C1_MBIT_ENABLE     // habilitar MMU (bit 0)
+    mcr     p15, 0, r0, c1, c0, 0           // escribir control register
+    pop { r4, pc }
+
+level1_table_addr:
     .word _page_table_start
 
-start_second_addr:
+level2_table_addr:
     .word _coarse_table_start
 
-secondary_table_addr:
-    .word secondary_table
+level2_table_entries_addr:
+    .word level2_table_entries
 
-main_table: 
+level1_table_entries: 
     .word  MMU_SECOND_TABLE_LOCATION | MMU_L1_COARSE_ENTRY
 
-secondary_table:
+level2_table_entries:
     .word (0x00011000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 0 (VA 0x00001000) (und)
     .word (0x00012000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 1 (VA 0x00002000) (abt)
     .word (0x00013000 & 0xFFFFF000) | (MMU_SP_AP_RW_RW | MMU_L2_SMALL_PAGE)  @ entrada 2 (VA 0x00003000) (irq)
