ba612baf4d2e7703e0a7b1de3ea3f3c88b6f0206
Ismael Salas López <isma.salas@protonmail.com>
2025-07-06 20:00:22 -0600

    Equal to stable-version branch

--- source/kernel.s
+++ source/kernel.s
@@ -7,20 +7,21 @@ main:
     mov     r2, #(MU_TRANSMITER_ENABLE | MU_RECEIVER_ENABLE)
     mov     r3, #0
     bl      uart0_Init
+    //bx      r0 
     
 loop:
     mov     r0, #1 
-    bl      uart0_Read
+    bl      uart0_read
     cmp     r0, #13
     movne   r1, #1 
-    blne    uart0_Write
+    blne    uart0_write
     bne     loop
     mov     r0, #'\r'
     mov     r1, #1 
-    bl      uart0_Write
+    bl      uart0_write
     mov     r0, #'\n'
     mov     r1, #1 
-    bl      uart0_Write
+    bl      uart0_write
     b       loop
 
 baudrate_speed:
