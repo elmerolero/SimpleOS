5c9a17d50d85ee388b27e8da051d9298b1722ebb
Ismael Salas López <isma.salas@protonmail.com>
2025-07-31 23:35:24 -0600

    EMMC Registers added

---
+++ source/devices/emmc.s
@@ -0,0 +1,28 @@
+@ EMMC Controller Registers
+.equ EMMC_ARG2_REG,         0x00
+.equ EMMC_BLKSIZECNT_REG,   0x04
+.equ EMMC_ARG1_REG,         0x08
+.equ EMMC_CMDTM_REG,        0x0C
+.equ EMMC_RESP0_REG,        0x10
+.equ EMMC_RESP1_REG,        0x14
+.equ EMMC_RESP2_REG,        0x18
+.equ EMMC_RESP3_REG,        0x1C
+.equ EMMC_DATA_REG,         0x20
+.equ EMMC_STATUS_REG,       0x24
+.equ EMMC_CONTROL0_REG,     0x28
+.equ EMMC_CONTROL1_REG,     0x2C
+.equ EMMC_INTERRUPT_REG,    0x30
+.equ EMMC_IRPT_MASK_REG,    0x34
+.equ EMMC_IRPT_EN_REG,      0x38
+.equ EMMC_CONTROL2_REG,     0x3C
+.equ EMMC_FORCE_IRPT_REG,   0x50
+.equ EMMC_BOOT_TIMEOUT_REG, 0x70
+.equ EMMC_DBG_SEL_REG,      0x74
+.equ EMMC_EXRDFIFO_CFG_REG, 0x80
+.equ EMMC_EXRDFIFO_EN_REG,  0x84
+.equ EMMC_TUNE_STEP_REG,    0x88
+.equ EMMC_TUNE_STEPS_STD,   0x8C
+.equ EMMC_TUNE_STEPS_DDR,   0x90
+.equ EMMC_SPI_INT_SPT,      0xF0
+.equ EMMS_SLOTISR_VER,      0xFC
+
